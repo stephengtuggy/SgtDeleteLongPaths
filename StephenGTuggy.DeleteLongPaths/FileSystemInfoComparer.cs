fb02bb2c4e972e4fb87fd88ceaef238f615a39ec
stephengtuggy <sgt@stephengtuggy.com>
2015-09-13 12:08:41 -0700

    No longer lists the same files or directories more than once when previewing files to delete.

---
+++ StephenGTuggy.DeleteLongPaths/FileSystemInfoComparer.cs
@@ -0,0 +1,47 @@
+﻿/**
+ * Copyright (C) 2015 Stephen G. Tuggy (sgt@StephenGTuggy.com).
+ * 
+ * This file is part of StephenGTuggy.DeleteLongPaths.
+ * 
+ * StephenGTuggy.DeleteLongPaths is free software: you can redistribute it 
+ * and/or modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.
+ *
+ * StephenGTuggy.DeleteLongPaths is distributed in the hope that it will be 
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General 
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with 
+ * StephenGTuggy.DeleteLongPaths.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace StephenGTuggy.DeleteLongPaths
+{
+    public class FileSystemInfoComparer : Comparer<Alphaleonis.Win32.Filesystem.FileSystemInfo>
+    {
+        public override int Compare(Alphaleonis.Win32.Filesystem.FileSystemInfo x, Alphaleonis.Win32.Filesystem.FileSystemInfo y)
+        {
+            if ((x == null) && (y == null))
+            {
+                return 0;
+            }
+            else if ((x == null) && (y != null))
+            {
+                return -1;
+            }
+            else if ((y == null) && (x != null))
+            {
+                return 1;
+            }
+            else
+            {
+                return StringComparer.Ordinal.Compare(x.FullName, y.FullName);
+            }
+        }
+    }
+}
--- StephenGTuggy.DeleteLongPaths/Program.cs
+++ StephenGTuggy.DeleteLongPaths/Program.cs
@@ -1,21 +1,24 @@
 ﻿/**
  * Copyright (C) 2015 Stephen G. Tuggy (sgt@StephenGTuggy.com).
  * 
- * This program is free software: you can redistribute it and/or modify it 
- * under the terms of the GNU General Public License as published by the Free 
- * Software Foundation, either version 3 of the License, or (at your option) 
- * any later version.
+ * This file is part of StephenGTuggy.DeleteLongPaths.
+ * 
+ * StephenGTuggy.DeleteLongPaths is free software: you can redistribute it 
+ * and/or modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation, either version 3 of the License, 
+ * or (at your option) any later version.
  *
- * StephenGTuggy.FuzzyMatch is distributed in the hope that it will be useful, 
- * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * StephenGTuggy.DeleteLongPaths is distributed in the hope that it will be 
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of 
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General 
  * Public License for more details.
  *
  * You should have received a copy of the GNU General Public License along with 
- * StephenGTuggy.FuzzyMatch.  If not, see <http://www.gnu.org/licenses/>.
+ * StephenGTuggy.DeleteLongPaths.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 using System;
+using System.Collections.Generic;
 
 namespace StephenGTuggy.DeleteLongPaths
 {
@@ -24,6 +27,7 @@ namespace StephenGTuggy.DeleteLongPaths
         public const string DefaultWildcard = "*.*";
         private const System.IO.FileAttributes FileAttributes1 = System.IO.FileAttributes.ReadOnly | System.IO.FileAttributes.Hidden | System.IO.FileAttributes.System;
         private const System.IO.FileAttributes FileAttributes2 = ~FileAttributes1;
+        private static readonly IComparer<Alphaleonis.Win32.Filesystem.FileSystemInfo> FileSystemInfoComparer = new FileSystemInfoComparer();
 
         public static void Main(string[] args)
         {
@@ -58,6 +62,8 @@ namespace StephenGTuggy.DeleteLongPaths
 
         public static void Delete(Alphaleonis.Win32.Filesystem.DirectoryInfo directoryToDelete, string wildcard, bool thisDirKnownToMatchWildcard, bool actuallyDelete)
         {
+            SortedSet<Alphaleonis.Win32.Filesystem.FileSystemInfo> fsis1 = null;
+            SortedSet<Alphaleonis.Win32.Filesystem.FileSystemInfo> fsis2 = null;
             if (!directoryToDelete.Exists)
             {
                 Console.WriteLine(string.Format("Directory \"{0}\" does not exist, or an error occurred trying to find it.", directoryToDelete.FullName));
@@ -67,24 +73,25 @@ namespace StephenGTuggy.DeleteLongPaths
                 // First, recursively delete this dir's child dirs and files that match the wildcard.
                 try
                 {
-                    foreach (var fsi in directoryToDelete.GetFileSystemInfos(wildcard, System.IO.SearchOption.TopDirectoryOnly))
+                    fsis1 = new SortedSet<Alphaleonis.Win32.Filesystem.FileSystemInfo>(directoryToDelete.GetFileSystemInfos(wildcard, System.IO.SearchOption.TopDirectoryOnly), FileSystemInfoComparer);
+                    foreach (var fsi1 in fsis1)
                     {
-                        if (fsi is Alphaleonis.Win32.Filesystem.DirectoryInfo)
+                        if (fsi1 is Alphaleonis.Win32.Filesystem.DirectoryInfo)
                         {
-                            var di = (Alphaleonis.Win32.Filesystem.DirectoryInfo)fsi;
-                            Delete(di, wildcard, true, actuallyDelete);
+                            var di1 = (Alphaleonis.Win32.Filesystem.DirectoryInfo)fsi1;
+                            Delete(di1, wildcard, true, actuallyDelete);
                         }
                         else
                         {
-                            var fi = (Alphaleonis.Win32.Filesystem.FileInfo)fsi;
+                            var fi1 = (Alphaleonis.Win32.Filesystem.FileInfo)fsi1;
                             try
                             {
                                 if (actuallyDelete)
                                 {
-                                    fi.Attributes &= FileAttributes2;
-                                    fi.Delete();
+                                    fi1.Attributes &= FileAttributes2;
+                                    fi1.Delete();
                                 }
-                                Console.WriteLine(string.Format("Deleted file \"{0}\".", fi.FullName));
+                                Console.WriteLine(string.Format("Deleted file \"{0}\".", fi1.FullName));
                             }
                             catch (System.IO.IOException ex)
                             {
@@ -115,7 +122,9 @@ namespace StephenGTuggy.DeleteLongPaths
                 {
                     try
                     {
-                        foreach (var fsi2 in directoryToDelete.GetFileSystemInfos())
+                        fsis2 = new SortedSet<Alphaleonis.Win32.Filesystem.FileSystemInfo>(directoryToDelete.GetFileSystemInfos(), FileSystemInfoComparer);
+                        fsis2.ExceptWith(fsis1);
+                        foreach (var fsi2 in fsis2)
                         {
                             if (fsi2 is Alphaleonis.Win32.Filesystem.DirectoryInfo)
                             {
@@ -189,9 +198,11 @@ namespace StephenGTuggy.DeleteLongPaths
                 {
                     try
                     {
-                        foreach (var di in directoryToDelete.GetDirectories())
+                        var dis3 = new SortedSet<Alphaleonis.Win32.Filesystem.FileSystemInfo>(directoryToDelete.GetDirectories(), FileSystemInfoComparer);
+                        dis3.ExceptWith(fsis1);
+                        foreach (var di in dis3)
                         {
-                            Delete(di, wildcard, false, actuallyDelete);
+                            Delete((Alphaleonis.Win32.Filesystem.DirectoryInfo)di, wildcard, false, actuallyDelete);
                         }
                     }
                     catch (UnauthorizedAccessException ex)
