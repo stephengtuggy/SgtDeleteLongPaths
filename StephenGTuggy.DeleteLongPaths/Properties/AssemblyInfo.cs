5cf0d8488b353b07ab76c467b849bc7551bc6fc7
Stephen G Tuggy <sgt@stephengtuggy.com>
2019-06-02 14:00:16 -0700

    chore: Versioning, product description, etc.

--- StephenGTuggy.DeleteLongPaths/Properties/AssemblyInfo.cs
+++ StephenGTuggy.DeleteLongPaths/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("StephenGTuggy.DeleteLongPaths")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Advanced Delete Tool for Windows")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("StephenGTuggy.DeleteLongPaths")]
-[assembly: AssemblyCopyright("Copyright © 2015-2016 Stephen G Tuggy")]
+[assembly: AssemblyProduct("SgtDeleteLongPaths")]
+[assembly: AssemblyCopyright("Copyright © 2015-2019 Stephen G Tuggy")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
